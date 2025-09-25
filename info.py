f477170b7cc0cd29ba8aacf360ed486630e1c87c
camilaqtrz <camilagureyes@gmail.com>
2025-09-24 23:53:21 -0400

    Add files via upload

---
+++ info.py
@@ -0,0 +1,68 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/me.jpeg"
+about_me = "Hi! I am Camila Gutierrez Reyes, a second year Biomedical Engineering major and Tech Promise Scholar!"
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/camila-gutierrez-reyes-486113337/"
+my_github_url = "https://github.com/camilaqtrz"
+my_email_address = "creyes45@gatech.edu"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Biomedical Engineering',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2028',
+    'GPA': '3.25'
+}
+course_data = {
+    "code":["CS 1301", "POL1101", "MATH1553", "BMED2110"], 
+    "names":["Intro to CS", "Government of the U.S", "Intro to Linear Algebra", "Conservation Principles in BME"], 
+    "semester_taken":["3rd", "2nd", "1st", "3rd"],
+    "skills":["Introduction to computing principles with emphasis on design and problem solving using programming tools.", "Foundations of law including civil rights and civil liberties. ", "Eigenvectors and eigenvalues are essential.", "Collaboration is key in problem solving."],
+    }
+experience_data = {
+    "Cashier at Food City / KVAT Foods Inc.": (["- Provided efficient service to customers at all times",
+                                                                          "- Scan and ring up customers according to company policy", "- Keep work area and area around work area clean"],"Images/foodcity.jpg"),
+    "Lead Sales Associate at Dollar General":(["- Provide expceptional customer service",
+                                                           "- Unload trucks according to described store process"],"Images/dg.jpg"),
+}
+
+projects_data = {
+    "Your Water Bottle": "A short inspired quiz based on Buzzfeed Quizzes! Based on your answers, it tell you which water bottle you are!",
+}
+
+programming_data = {
+    "Python": 30
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍"
+}
+spoken_icons = {
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "Spanish": "Read and spoken at home",
+}
+
+leadership_data = {
+    "Leadership Role in Medical Robotics (Future Goal)": ["-"]
+
+}
+activity_data={
+    "Medical Robotics Club": ["- Participated in subunit: Robotics 101", 
+            "- Created a robotic arm using Solidworks for parts and Arduino to program movement"]
+}
